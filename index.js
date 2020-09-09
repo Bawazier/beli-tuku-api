d30983d137859039b78b913e050b475af021302d
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-09-09 22:46:49 +0700

    select all data items with method GET

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./src/helper/db');
+const qs = require('querystring');
 
 const app = express();
 
@@ -15,17 +16,96 @@ app.post('/items', (req, res) => {
         db.query(`INSERT INTO items(name, price, description) VALUES ('${name}', ${price}, '${description}')`, (err, results, fields) => {
             if(!err){
                 res.status(201).send({
-                    status: true,
+                    success: true,
                     message: 'Insert Data Success',
                     data: req.body
                 });
             }else{
                 console.log(err);
                 res.status(500).send({
-                    status: false,
-                    message: 'Insert Data Fialled'
+                    success: false,
+                    message: 'Insert Data Failled'
                 });
             }
         });
+    }else{
+        res.status(400).send({
+            success: false,
+            message: 'Input data Failled'
+        });
     }
 });
+
+app.get('/items', (req, res) => {
+    let {page, limit, search} = req.query;
+    let searchKey = '';
+    let searchValue = '';
+    if(typeof(search) === 'object'){
+        searchKey = Object.keys(search) [0];
+        serachValue = Object.values(search) [0];
+    }else{
+        searchKey = 'name';
+        serachValue = search || '';
+    }
+
+    if(!limit){
+        limit = 5;
+    }else{
+        limit = parseInt(limit);
+    }
+
+    if(!page){
+        page = 1;
+    }else{
+        page = parseInt(page);
+    }
+
+    const offset = (page-1) * limit;
+
+    db.query(`SELECT * FROM items WHERE ${searchKey} LIKE '%${searchValue}%' 
+    LIMIT ${limit} OFFSET ${offset}`, (err, results, fields) => {
+        if(!err){
+            const pageInfo = {
+                count: 0,
+                pages: 0,
+                currentPage: page,
+                limitPage: limit,
+                nextLink: null,
+                prevLink: null 
+            }
+
+            if(results.length){
+                db.query(`SELECT COUNT(*) AS count FROM items WHERE ${searchKey} LIKE '%${searchValue}%'`, 
+                (err, data, fields) => {
+                    const {count} = data[0];
+                    pageInfo.count = count;
+                    pageInfo.pages = Math.ceil(count / limit);
+
+                    const {pages, currentPage} = pageInfo;
+
+                    if(currentPage < pages){
+                        pageInfo.nextLink = `http://localhost:5000/items?${qs.stringify({ ...req.query, ...{page: page+1}})}`;
+                    }
+
+                    if(currentPage > 1){
+                        pageInfo.prevLink = `http://localhost:5000/items?${qs.stringify({ ...req.query, ...{page: page-1}})}`;
+                    }
+
+                    res.send({
+                        success: true,
+                        message: 'List of items',
+                        data: results,
+                        pageInfo
+                    });
+                })
+            }else{
+                res.send({
+                    success: false,
+                    message: 'List of items'
+                });
+            }
+        }else{
+            console.log(err);
+        }
+    });
+})
