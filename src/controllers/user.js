fb6fcaae73aa8edf90f012f9e3c0cccd27dfd315
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-09-13 06:29:19 +0700

    CRUD user MVC

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -34,7 +34,99 @@ module.exports = {
 				}
 			});		
 		}
+	},
 
-		
-	}
+	updateAll: (req, res) => {
+		const user = {
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password,
+			phone: req.body.phone,
+			gender: req.body.gender,
+			dateOfBirth: req.body.dateOfBirth
+		};
+
+		if (!user.name || !user.email || !user.password || !user.phone) {
+			res.status(400).send({
+				message: 'Content can not be empty!',
+			});
+		}else{
+			User.updateAll(user, req.params.id, (err, data) => {
+				if (!err) {
+					res.status(201).send({
+						success: true,
+						message: 'Update Data Success',
+						data: { ...user }
+					});
+				} else {
+					console.log(err);
+					res.status(500).send({
+						success: false,
+						message: 'Update Data Failled',
+						data: data
+					});
+				}
+			});
+		}
+	},
+
+	findById: (req, res) => {
+		User.findById(req.params.id, (err, data) => {
+			if (!err) {
+				res.status(201).send({
+					success: true,
+					message: 'SELECT BY ID SUCCESS',
+					data: data
+				});
+			} else {
+				if (err.kind === 'not_found') {
+					res.status(404).send({
+						message: `Not found User with id ${req.params.id}.`,
+					});
+				} else {
+					res.status(500).send({
+						message: 'Error retrieving User with id ' + req.params.id,
+					});
+				}
+			}
+		});
+	},
+
+	deleteById: (req, res) => {
+		User.deleteById(req.params.id, (err, data) => {
+			if(!err){
+				res.status(201).send({
+					success: true,
+					message: `Delete ${req.params.id} Success`,
+					data: data
+				});
+			}else{
+				if (err.kind === 'not_found') {
+					res.status(404).send({
+						message: `Not found User with id ${req.params.id}.`,
+					});
+				} else {
+					res.status(500).send({
+						message: 'Error retrieving User with id ' + req.params.id,
+					});
+				}
+			}
+		});
+	},
+
+	findAll: (req, res) => {
+		User.findAll((err, data) => {
+			if (!err) {
+				res.status(201).send({
+					success: true,
+					message: 'SELECT ALL SUCCESS',
+					data: data
+				});
+			} else {
+				res.status(500).send({
+					message: 'Error retrieving User with id ' + req.params.id,
+				});
+			}
+		});
+	},
 };
--- src/models/user.js
+++ src/models/user.js
@@ -23,4 +23,50 @@ User.create = (user, result) => {
 	});
 };
 
+User.findById = (id, result) => {
+	db.query(`SELECT * FROM ${tableName} WHERE id=?`, [id], (err, res) => {
+		if(!err) {
+			if(res.length){
+				result(null, res);
+			}else{
+				result({ kind: 'not_found' }, null);
+			}
+		}else{
+			result(err, null);
+		}
+	});
+};
+
+User.updateAll = (user, id, result) => {
+	db.query(`UPDATE ${tableName} SET name=?, email=?, password=?, phone=?, gender=?, dateOfBirth=? 
+	WHERE id=?`, [user.name, user.email, user.password, user.phone, user.gender, user.dateOfBirth, id], 
+	(err, res) => {
+		if(!err){
+			result(null, {...user});
+		}else{
+			result(err, res);
+		}
+	});
+};
+
+User.findAll = (result) =>{
+	db.query(`SELECT * FROM ${tableName}`, (err, res) => {
+		if(!err){
+			result(null, res);
+		}else{
+			result(err, null);
+		}
+	});
+};
+
+User.deleteById = (id, result) => {
+	db.query(`DELETE FROM ${tableName} WHERE id = ?`, [id], (err, res) => {
+		if(!err){
+			result(null, res);
+		}else{
+			result(err, null);
+		}
+	});
+};
+
 module.exports = User;
