753a1baf510fa32fca1a33bf2b96d9f2609fa55a
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-09-13 07:28:43 +0700

    CRUD cart MVC

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ app.listen(5000, () => {
 const productRouter = require('./src/routes/product');
 const categoryRouter = require('./src/routes/category');
 const userRouter = require('./src/routes/user');
+const cartRouter = require('./src/routes/cart');
 
 // attach member router
 app.use('/product', productRouter);
 app.use('/category', categoryRouter);
+app.use('/cart', cartRouter);
 app.use('/user', userRouter);
