01cba01f9cf134c84827176d49c9da2b0fa34115
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-17 19:08:08 +0700

    fix address controller

--- src/controllers/address.js
+++ src/controllers/address.js
@@ -1,136 +1,144 @@
-const Address = require('../models/userAddress');
-const responeStandart = require('../helper/respone');
-const schema = require('../helper/inputValidated');
+const { UserAddress, User } = require("../models");
+const { Op } = require("sequelize");
+const responseStandart = require("../helper/response");
+const schema = require("../helper/validation");
 
-const addressSchema = schema.profileCustomer[1];
-
+const addressSchema = schema.Address;
 
 module.exports = {
-	findAddressByUserId: (req, res) => {
-		Address.findByUserId(req.user.id, (err, respone) => {
-			if (!err) {
-				return responeStandart(res, 'get user address success', { respone });
-			} else {
-				if (err.kind === 'not_found') {
-					return responeStandart(res, 'address not found', {}, 404, false);
-				} else {
-					return responeStandart(res, err.sqlMessage, {}, 500, false);
-				}
-			}
-		});
-	},
+  getAddress: async (req, res) => {
+    try {
+      const { count, rows } = await UserAddress.findAndCountAll({
+        include: [{ model: User, attributes: ["name", "picture"] }],
+        where: {
+          userId: req.user.id,
+          name: {
+            [Op.startsWith]: req.query.search || "",
+          },
+        },
+        order: [["isPrimary", "DESC"]],
+        offset: parseInt(req.query.page) || 0,
+        limit: parseInt(req.query.limit) || 10,
+      });
+      if (rows.length) {
+        return responseStandart(res, "success to display address", {
+          rows,
+          pageInfo: [
+            {
+              count: count,
+              page: parseInt(req.query.page) || 0,
+              limit: parseInt(req.query.limit) || 10,
+            },
+          ],
+        });
+      } else {
+        return responseStandart(
+          res,
+          "unable to display address",
+          {
+            pageInfo: [
+              {
+                count: count,
+                page: parseInt(req.query.page) || 0,
+                limit: parseInt(req.query.limit) || 10,
+              },
+            ],
+          },
+          400,
+          false
+        );
+      }
+    } catch (e) {
+      return responseStandart(
+        res,
+        "unable to display address",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
 
-	createAddressByUserId: async (req, res) => {
-		try {
-			const result = await addressSchema.required().validateAsync(req.body);
-			const address = {
-				user_id: req.user.id,
-				name: result.name,
-				recipient_name: result.recipient_name,
-				recipient_tlp: result.recipient_tlp,
-				address: result.address,
-				region: result.region,
-				postal_code: result.postal_code,
-				isPrimary: 0
-			};
+  getAddressId: async (req, res) => {
+    try {
+      const results = await UserAddress.findByPk(req.params.id);
+      if (results) {
+        return responseStandart(res, "success to display address", {
+          results,
+        });
+      } else {
+        return responseStandart(
+          res,
+          "unable to display address",
+          {},
+          400,
+          false
+        );
+      }
+    } catch (e) {
+      return responseStandart(
+        res,
+        "unable to display address",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
 
-			Address.create(address, (err, respone) => {
-				if (!err) {
-					return responeStandart(res, 'create new address success', {respone});
-				} else {
-					return responeStandart(res, err.sqlMessage, {}, 500, false);
-				}
-			});
-		} catch (e) {
-			return responeStandart(res, e.details[0].message, {}, 400, false);
-		}
-	},
+  postAddress: async (req, res) => {
+    try {
+      const result = await addressSchema.required().validateAsync(req.body);
+      const address = {
+        userId: req.user.id,
+        name: result.name,
+        recipientName: result.recipientName,
+        recipientTlp: result.recipientTlp,
+        address: result.address,
+        region: result.region,
+        postalCode: result.postalCode,
+        isPrimary: 0,
+      };
+      await UserAddress.create(address);
+      return responseStandart(res, "success create your shipping address", {});
+    } catch (e) {
+      return responseStandart(
+        res,
+        "failed for create story",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
 
-	updateAddressByUserId: async (req, res) => {
-		try {
-			const result = await addressSchema.validateAsync(req.body);
-			const address = {
-				user_id: req.user.id,
-				name: result.name,
-				recipient_name: result.recipient_name,
-				recipient_tlp: result.recipient_tlp,
-				address: result.address,
-				region: result.region,
-				postal_code: result.postal_code,
-				isPrimary: 0
-			};
-			let filteredObject = Object.keys(address).reduce((result, key) => {
-				if (address[key] !== undefined) result[key] = address[key];
-				return result;
-			}, {});
-
-			Address.updateByUserId(filteredObject, req.params.id, (err, respone) => {
-				if (!err) {
-					return responeStandart(res, 'change address success', { respone });
-				} else {
-					if (err.kind === 'not_found') {
-						return responeStandart(res, 'address not found', {}, 404, false);
-					} else {
-						return responeStandart(res, err.sqlMessage, {}, 500, false);
-					}
-				}
-			});
-		} catch (e) {
-			return responeStandart(res, e.details[0].message, {}, 400, false);
-		}
-	},
-
-	updateAddressAllByUserId: async (req, res) => {
-		try {
-			const result = await addressSchema.required().validateAsync(req.body);
-			const address = {
-				user_id: req.user.id,
-				name: result.name,
-				recipient_name: result.recipient_name,
-				recipient_tlp: result.recipient_tlp,
-				address: result.address,
-				region: result.region,
-				postal_code: result.postal_code,
-				isPrimary: 0
-			};
-
-			Address.updateByUserId(address, req.params.id, (err, respone) => {
-				if (!err) {
-					return responeStandart(res, 'change address success', { respone });
-				} else {
-					if (err.kind === 'not_found') {
-						return responeStandart(res, 'address not found', {}, 404, false);
-					} else {
-						return responeStandart(res, err.sqlMessage, {}, 500, false);
-					}
-				}
-			});
-		} catch (e) {
-			return responeStandart(res, e.details[0].message, {}, 400, false);
-		}
-	},
-
-	updatePrimaryAddress: async (req, res) => {
-		try {
-			const result = await addressSchema.required().validateAsync(req.body);
-			const address = {
-				user_id: req.user.id,
-				isPrimary: result.isPrimary === 'true' ? 1:0
-			};
-
-			Address.updatePrimary(address, req.params.id, (err) => {
-				if (!err) {
-					return responeStandart(res, 'change primary address success', {});
-				} else {
-					if (err.kind === 'not_found') {
-						return responeStandart(res, 'address not found', {}, 404, false);
-					} else {
-						return responeStandart(res, err.sqlMessage, {}, 500, false);
-					}
-				}
-			});
-		} catch (e) {
-			return responeStandart(res, e.details[0].message, {}, 400, false);
-		}
-	}
+  patchAddress: async (req, res) => {
+    try {
+      const result = await addressSchema.validateAsync(req.body);
+      const address = {
+        userId: req.user.id,
+        name: result.name,
+        recipientName: result.recipientName,
+        recipientTlp: result.recipientTlp,
+        address: result.address,
+        region: result.region,
+        postalCode: result.postalCode,
+        isPrimary: result.isPrimary ? 1:0,
+      };
+      await UserAddress.update(address, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      return responseStandart(res, "success update your shipping address", {});
+    } catch (e) {
+      return responseStandart(
+        res,
+        "failed for update story",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
 };
