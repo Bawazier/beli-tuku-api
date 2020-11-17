27e679f0ffe98ba5d34db9d541567d230e86c624
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-17 19:06:55 +0700

    fix home controller

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,128 +1,327 @@
-const Product = require('../models/product');
-const Category = require('../models/category');
-const Images = require('../models/productImages');
-const responeStandart = require('../helper/respone');
+const {
+  Product,
+  Category,
+  Condition,
+  User,
+  ProductColor,
+  ProductImage,
+  ProductRating,
+} = require("../models");
+const responseStandart = require("../helper/response");
+const { Op } = require("sequelize");
 
 module.exports = {
-	findProductBySearch: (req, res) => {
-		let { search } = req.query;
-		let searchKey = '';
-		let searchValue = '';
-		if (typeof (search) === 'object') {
-			searchKey = Object.keys(search)[0];
-			searchValue = Object.values(search)[0];
-		} else {
-			searchKey = 'products.name';
-			searchValue = search || '';
-		}
-		
-		Product.findBySearch(searchKey, searchValue, (err, respone) => {
-			let data = respone.map(item => item.id);
-			Images.findImageByPrimary(data, (err, image) => {
-				let imagesPrimary = image.map(getImage => {
-					return {
-						id_product: getImage.product_id,
-						URL_image: process.env.URL + getImage.picture
-					};
-				});
-				if(!err){
-					return responeStandart(res, `search ${searchValue}`, {respone, imagesPrimary});
-				}else{
-					if (err.kind === 'not_found') {
-						return responeStandart(res, `Not found Product with ${searchValue}.`, {}, 404, false);
-					} else {
-						return responeStandart(res, err.sqlMessage, {}, 500, false);
-					}
-				}
-			});
-		});	
+  searchProduct: async (req, res) => {
+    try {
+      const { count, rows } = await Product.findAndCountAll({
+        attributes: [
+          "id",
+          "name",
+          "price",
+          "stock",
+          "description",
+          "createdAt",
+          "updatedAt",
+        ],
+        include: [
+          { model: Category, attributes: ["name", "picture", "color"] },
+          { model: Condition, attributes: ["status"] },
+          { model: User, attributes: ["name", "picture"] },
+          { model: ProductColor, attributes: ["hexa"] },
+          { model: ProductImage, attributes: ["picture", "isPrimary"] },
+          { model: ProductRating, attributes: ["userId", "rating", "comment"] },
+        ],
+        where: {
+          name: {
+            [Op.startsWith]: req.query.search,
+          },
+        },
+        order: [["createdAt", "DESC"]],
+        offset: parseInt(req.query.page) || 0,
+        limit: parseInt(req.query.limit) || 10,
+      });
+      if (rows.length) {
+        return responseStandart(res, "success to display products", {
+          rows,
+          pageInfo: [
+            {
+              count: count,
+              page: parseInt(req.query.page) || 0,
+              limit: parseInt(req.query.limit) || 10,
+            },
+          ],
+        });
+      } else {
+        return responseStandart(
+          res,
+          "unable to display products",
+          {
+            pageInfo: [
+              {
+                count: count,
+                page: parseInt(req.query.page) || 0,
+                limit: parseInt(req.query.limit) || 10,
+              },
+            ],
+          },
+          400,
+          false
+        );
+      }
+    } catch (e) {
+      console.log(e);
+      return responseStandart(
+        res,
+        "unable to display products",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
 
-	},
+  searchNews: async (req, res) => {
+    try {
+      const { count, rows } = await Product.findAndCountAll({
+        attributes: [
+          "id",
+          "name",
+          "price",
+          "stock",
+          "description",
+          "createdAt",
+          "updatedAt",
+        ],
+        include: [
+          { model: Category, attributes: ["name", "picture", "color"] },
+          { model: Condition, attributes: ["status"] },
+          { model: User, attributes: ["name", "picture"] },
+          { model: ProductColor, attributes: ["hexa"] },
+          { model: ProductImage, attributes: ["picture", "isPrimary"] },
+          { model: ProductRating, attributes: ["userId", "rating", "comment"] },
+        ],
+        order: [["createdAt", "DESC"]],
+        offset: parseInt(req.query.page) || 0,
+        limit: parseInt(req.query.limit) || 10,
+      });
+      if (rows.length) {
+        return responseStandart(res, "success to display products", {
+          rows,
+          pageInfo: [
+            {
+              count: count,
+              page: parseInt(req.query.page) || 0,
+              limit: parseInt(req.query.limit) || 10,
+            },
+          ],
+        });
+      } else {
+        return responseStandart(
+          res,
+          "unable to display products",
+          {
+            pageInfo: [
+              {
+                count: count,
+                page: parseInt(req.query.page) || 0,
+                limit: parseInt(req.query.limit) || 10,
+              },
+            ],
+          },
+          400,
+          false
+        );
+      }
+    } catch (e) {
+      console.log(e);
+      return responseStandart(
+        res,
+        "unable to display products",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
+  
+  searchPopular: async (req, res) => {
+    try {
+      const { count, rows } = await Product.findAndCountAll({
+        attributes: [
+          "id",
+          "name",
+          "price",
+          "stock",
+          "description",
+          "createdAt",
+          "updatedAt",
+        ],
+        include: [
+          { model: Category, attributes: ["name", "picture", "color"] },
+          { model: Condition, attributes: ["status"] },
+          { model: User, attributes: ["name", "picture"] },
+          { model: ProductColor, attributes: ["hexa"] },
+          { model: ProductImage, attributes: ["picture", "isPrimary"] },
+          { model: ProductRating, attributes: ["userId", "rating", "comment"] },
+        ],
+        order: [[ProductRating, "rating", "DESC"]],
+        offset: parseInt(req.query.page) || 0,
+        limit: parseInt(req.query.limit) || 10,
+      });
+      if (rows.length) {
+        return responseStandart(res, "success to display products", {
+          rows,
+          pageInfo: [
+            {
+              count: count,
+              page: parseInt(req.query.page) || 0,
+              limit: parseInt(req.query.limit) || 10,
+            },
+          ],
+        });
+      } else {
+        return responseStandart(
+          res,
+          "unable to display products",
+          {
+            pageInfo: [
+              {
+                count: count,
+                page: parseInt(req.query.page) || 0,
+                limit: parseInt(req.query.limit) || 10,
+              },
+            ],
+          },
+          400,
+          false
+        );
+      }
+    } catch (e) {
+      console.log(e);
+      return responseStandart(
+        res,
+        "unable to display products",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
 
-	findCategory: (req, res) => {
-		Category.findAll((err, respone) => {
-			if (!err) {
-				const data = respone.map((item) => {
-					const picture = { URL_image: process.env.URL + item.picture };
-					return Object.assign({}, item, picture);
-				});
-				return responeStandart(res, 'get category', { data });
-			} else {
-				if (err.kind === 'not_found') {
-					return responeStandart(res, 'not found category', {}, 404, false);
-				} else {
-					return responeStandart(res, err.sqlMessage, {}, 500, false);
-				}
-			}
-		});
-	},
-	
-	findProductByCategoryId: (req, res) => {
-		Product.findByCategoryId(req.params.id, (err, respone) => {
-			let data = respone.map(item => item.id);
-			Images.findImageByPrimary(data, (err, image) => {
-				let imagesPrimary = image.map(getImage => {
-					return {
-						id_product: getImage.product_id,
-						URL_image: process.env.URL + getImage.picture
-					};
-				});
-				if(!err){
-					return responeStandart(res, `select product with category id ${req.params.id} success`, {respone, imagesPrimary});
-				}else{
-					if (err.kind === 'not_found') {
-						return responeStandart(res, `Not found Product with category id ${req.params.id}.`, {}, 404, false);
-					} else {
-						return responeStandart(res, err.sqlMessage, {}, 500, false);
-					}
-				}
-			});
-		});
-	},
+  searchCategory: async (req, res) => {
+    try {
+      const { count, rows } = await Product.findAndCountAll({
+        attributes: [
+          "id",
+          "name",
+          "price",
+          "stock",
+          "description",
+          "createdAt",
+          "updatedAt",
+        ],
+        include: [
+          { model: Category, attributes: ["name", "picture", "color"] },
+          { model: Condition, attributes: ["status"] },
+          { model: User, attributes: ["name", "picture"] },
+          { model: ProductColor, attributes: ["hexa"] },
+          { model: ProductImage, attributes: ["picture", "isPrimary"] },
+          { model: ProductRating, attributes: ["userId", "rating", "comment"] },
+        ],
+        where: {
+          categoryId: req.params.id,
+        },
+        order: [["createdAt", "DESC"]],
+        offset: parseInt(req.query.page) || 0,
+        limit: parseInt(req.query.limit) || 10,
+      });
+      if (rows.length) {
+        return responseStandart(res, "success to display products", {
+          rows,
+          pageInfo: [
+            {
+              count: count,
+              page: parseInt(req.query.page) || 0,
+              limit: parseInt(req.query.limit) || 10,
+            },
+          ],
+        });
+      } else {
+        return responseStandart(
+          res,
+          "unable to display products",
+          {
+            pageInfo: [
+              {
+                count: count,
+                page: parseInt(req.query.page) || 0,
+                limit: parseInt(req.query.limit) || 10,
+              },
+            ],
+          },
+          400,
+          false
+        );
+      }
+    } catch (e) {
+      console.log(e);
+      return responseStandart(
+        res,
+        "unable to display products",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
 
-	findProductSortByCreatedAt: (req, res) => {
-		Product.findByCreatedAt((err, respone) => {
-			let data = respone.map(item => item.id);
-			Images.findImageByPrimary(data, (err, image) => {
-				let imagesPrimary = image.map(getImage => {
-					return {
-						id_product: getImage.product_id,
-						URL_image: process.env.URL + getImage.picture
-					};
-				});
-				if(!err){
-					return responeStandart(res, 'find new products success', {respone, imagesPrimary});
-				}else{
-					if (err.kind === 'not_found') {
-						return responeStandart(res, 'not found new products', {}, 404, false);
-					} else {
-						return responeStandart(res, err.sqlMessage, {}, 500, false);
-					}
-				}
-			});
-		});
-	},
-
-	findProductSortByRatings: (req, res) => {
-		Product.findByRatings((err, respone) => {
-			let data = respone.map(item => item.id);
-			Images.findImageByPrimary(data, (err, image) => {
-				let imagesPrimary = image.map(getImage => {
-					return {
-						id_product: getImage.product_id,
-						URL_image: process.env.URL + getImage.picture
-					};
-				});
-				if(!err){
-					return responeStandart(res, 'find populer products success', {respone, imagesPrimary});
-				}else{
-					if (err.kind === 'not_found') {
-						return responeStandart(res, 'not found populer products', {}, 404, false);
-					} else {
-						return responeStandart(res, err.sqlMessage, {}, 500, false);
-					}
-				}
-			});
-		});
-	},
+  findCategory: async (req, res) => {
+    try {
+      const { count, rows } = await Category.findAndCountAll({
+        where: {
+          name: {
+            [Op.startsWith]: req.query.search || "",
+          },
+        },
+        offset: parseInt(req.query.page) || 0,
+        limit: parseInt(req.query.limit) || 10,
+      });
+      if (rows.length) {
+        return responseStandart(res, "success to display categories", {
+          results: rows,
+          pageInfo: [
+            {
+              count: count,
+              page: parseInt(req.query.page) || 0,
+              limit: parseInt(req.query.limit) || 10,
+            },
+          ],
+        });
+      } else {
+        return responseStandart(
+          res,
+          "unable to display categories",
+          {
+            pageInfo: [
+              {
+                count: count,
+                page: parseInt(req.query.page) || 0,
+                limit: parseInt(req.query.limit) || 10,
+              },
+            ],
+          },
+          400,
+          false
+        );
+      }
+    } catch (e) {
+      return responseStandart(
+        res,
+        "unable to display categories",
+        { ValidationError: e.details[0].message, sqlError: e },
+        400,
+        false
+      );
+    }
+  },
 };
